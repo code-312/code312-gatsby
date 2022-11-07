f76052e8094ce70e15c4b68448d44e676625cf27
Dior.Shelton <dior.shelton@gmail.com>
2022-11-07 08:03:42 -0500

    Move svg to alt folder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
+      <img src="../images" alt="" />
       <p>
         <a href="https://codeforamerica.org/" target="_blank" rel="noreferrer">Code for America Labs, Inc.</a> is a non-partisan, non-political 501(c)(3) charitable organization</p>
     </footer>
