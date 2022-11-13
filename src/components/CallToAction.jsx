4113b8d74b1ddaa4ff4241aca051f9e6b1f56272
Edgar Cuarezma <ecuarezma@gmail.com>
2022-11-13 13:19:36 -0600

    Create and write html for Call-to-action component

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const CallToAction = () => (
+  <section>
+    <h1>Have you completed the pre-onboarding checklist?</h1>
+    <Link to="">Start onboarding today!</Link>
+  </section>
+)
+
+export default CallToAction
