4bb5c79a4509b62cf6227090d54164cd6aecf2a8
Dior.Shelton <dior.shelton@gmail.com>
2022-11-20 20:26:17 -0500

    Correct formatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React from 'react'
 import styled from 'styled-components'
-import logo from "../images/cfc_logo_short.svg"
+import logo from '../images/cfc_logo_short.svg'
 
 const StyledFooter = styled.footer`
-width: 14rem;
+  width: 14rem;
 `
 const Footer = () => {
   return (
     <StyledFooter>
-      <img src={logo} alt="Code for Chicago Logo Icon"/>
+      <img src={logo} alt="Code for Chicago Logo Icon" />
       <p>
-        <a href="https://codeforamerica.org/" target="_blank" rel="noreferrer">Code for America Labs, Inc.</a> is a non-partisan, non-political 501(c)(3) charitable organization</p>
+        <a href="https://codeforamerica.org/" target="_blank" rel="noreferrer">
+          Code for America Labs, Inc.
+        </a>{' '}
+        is a non-partisan, non-political 501(c)(3) charitable organization
+      </p>
     </StyledFooter>
   )
 }
