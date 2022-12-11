e4885649290f61e4beed1d8994277a32a7c0ce17
Edgar Cuarezma <ecuarezma@gmail.com>
2022-12-11 13:21:34 -0600

    add styled component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from "styled-components"
 
-const Button = () => (
-  <button>click me</button>
+const StyledButton = styled.button``
+
+const Button = ({text}) => (
+  <StyledButton>{text}</StyledButton>
 )
+
+export default Button;
