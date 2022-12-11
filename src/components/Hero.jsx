83548d38781d506377e86bdf7fb6ce1b62a0da49
Sergen Demir <dmrsrgn@gmail.com>
2022-12-11 11:52:00 -0600

    convert px to rem units & add 'Styled' to styled component names

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const HeroSection = styled.section`
+const StyledHeroSection = styled.section`
   background-color: #b3ddf2;
 
   @media (min-width: 769px) {
     display: flex;
     align-items: center;
-    padding-left: 48px;
-    gap: 24px;
+    padding-left: 3rem;
+    gap: 1.5rem;
   }
 `
 
-const ContentContainer = styled.div`
+const StyledContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 16px;
-  padding: 48px 24px;
+  gap: 1rem;
+  padding: 3rem 1.5rem;
 
   @media (min-width: 769px) {
-    display: flex;
-    flex-direction: column;
     align-items: flex-start;
-    padding: 64px 0px;
-    gap: 24px;
+    padding: 4rem 0px;
+    gap: 1.5rem;
     width: 499px;
     height: 330px;
   }
@@ -33,7 +31,7 @@ const ContentContainer = styled.div`
   }
 `
 
-const EyebrowText = styled.span`
+const StyledEyebrowText = styled.span`
   font-style: normal;
   font-weight: 600;
   letter-spacing: 0.05em;
@@ -45,36 +43,35 @@ const EyebrowText = styled.span`
   color: #001426;
 
   @media (min-width: 649px) {
-    font-size: 18px;
+    font-size: 1.125rem;
   }
 `
-const Heading = styled.h2`
+const StyledHeading = styled.h2`
   text-align: center;
-  font-size: 32px;
+  font-size: 2rem;
   font-weight: 700;
   color: #001426;
-  font-family: 'Source Sans Pro';
   flex: none;
 
   @media (min-width: 649px) {
-    font-size: 42px;
+    font-size: 2.625rem;
   }
   @media (min-width: 769px) {
     text-align: left;
   }
 `
 
-const HeroImageContainer = styled.div`
+const StyledHeroImageContainer = styled.div`
   @media (min-width: 769px) {
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
     align-items: flex-start;
     padding: 0px;
-    gap: 10px;
+    gap: 0.625rem;
     width: 198px;
     height: 421px;
-    padding-top: 64px;
+    padding-top: 4rem;
   }
 
   @media (min-width: 1200px) {
@@ -82,7 +79,7 @@ const HeroImageContainer = styled.div`
   }
 `
 
-const HeroImage = styled.img`
+const StyledHeroImage = styled.img`
   @media (min-width: 769px) {
     width: 100%;
     height: 100%;
@@ -91,18 +88,18 @@ const HeroImage = styled.img`
   }
 `
 
-const Button = styled.button`
-  padding: 8px 16px;
+const StyledButton = styled.button`
+  padding: 0.5rem 1rem;
   background-color: #ffffff;
   border: 2px solid #d90000;
   box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
   border-radius: 2.2px;
   color: #001426;
   font-weight: 600;
-  font-size: 14px;
+  font-size: 0.875rem;
 
   @media (min-width: 649px) {
-    font-size: 16px;
+    font-size: 1rem;
   }
 `
 
@@ -114,16 +111,16 @@ const Hero = ({
   buttonText,
 }) => {
   return (
-    <HeroSection>
-      <ContentContainer>
-        <EyebrowText>{eyebrowText}</EyebrowText>
-        <Heading>{mainHeading}</Heading>
-        <Button>{buttonText}</Button>
-      </ContentContainer>
-      <HeroImageContainer>
-        <HeroImage src={img} alt={imgDescription} />
-      </HeroImageContainer>
-    </HeroSection>
+    <StyledHeroSection>
+      <StyledContentContainer>
+        <StyledEyebrowText>{eyebrowText}</StyledEyebrowText>
+        <StyledHeading>{mainHeading}</StyledHeading>
+        <StyledButton>{buttonText}</StyledButton>
+      </StyledContentContainer>
+      <StyledHeroImageContainer>
+        <StyledHeroImage src={img} alt={imgDescription} />
+      </StyledHeroImageContainer>
+    </StyledHeroSection>
   )
 }
 
