a2113f9c989b6234fa273d8fa3ee77aeab47a85a
Sergen Demir <dmrsrgn@gmail.com>
2022-12-26 11:20:18 -0600

    convert px to rem units. use css variables instead of hex codes. add placeholder text.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import heroImg from '../images/skyline.svg'
 
 const StyledHeroSection = styled.section`
-  background-color: #b3ddf2;
+  background-color: var(--cfa-hero-bg);
 
   @media (min-width: 769px) {
     display: flex;
@@ -21,13 +22,13 @@ const StyledContentContainer = styled.div`
 
   @media (min-width: 769px) {
     align-items: flex-start;
-    padding: 4rem 0px;
+    padding: 4rem 0;
     gap: 1.5rem;
-    width: 499px;
-    height: 330px;
+    width: 31.188rem;
+    height: 20.625rem;
   }
   @media (min-width: 1200px) {
-    width: 540px;
+    width: 33.75rem;
   }
 `
 
@@ -35,12 +36,12 @@ const StyledEyebrowText = styled.span`
   font-style: normal;
   font-weight: 600;
   letter-spacing: 0.05em;
-  line-height: 21px;
+  line-height: 1.313rem;
   text-align: center;
   letter-spacing: 0.05em;
   text-transform: uppercase;
-  height: 21px;
-  color: #001426;
+  height: 1.313rem;
+  color: var(--tints-blizzard-black);
 
   @media (min-width: 649px) {
     font-size: 1.125rem;
@@ -50,7 +51,8 @@ const StyledHeading = styled.h2`
   text-align: center;
   font-size: 2rem;
   font-weight: 700;
-  color: #001426;
+  color: var(--tints-blizzard-black);
+  line-height: 3.438rem;
   flex: none;
 
   @media (min-width: 649px) {
@@ -67,15 +69,15 @@ const StyledHeroImageContainer = styled.div`
     flex-direction: column;
     justify-content: flex-end;
     align-items: flex-start;
-    padding: 0px;
+    padding: 0;
     gap: 0.625rem;
-    width: 198px;
-    height: 421px;
+    width: 12.375rem;
+    height: 26.313rem;
     padding-top: 4rem;
   }
 
   @media (min-width: 1200px) {
-    width: 612px;
+    width: 38.25rem;
   }
 `
 
@@ -90,11 +92,11 @@ const StyledHeroImage = styled.img`
 
 const StyledButton = styled.button`
   padding: 0.5rem 1rem;
-  background-color: #ffffff;
+  background-color: var(--white);
   border: 2px solid #d90000;
   box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
   border-radius: 2.2px;
-  color: #001426;
+  color: var(--tints-blizzard-black);
   font-weight: 600;
   font-size: 0.875rem;
 
@@ -113,12 +115,16 @@ const Hero = ({
   return (
     <StyledHeroSection>
       <StyledContentContainer>
-        <StyledEyebrowText>{eyebrowText}</StyledEyebrowText>
-        <StyledHeading>{mainHeading}</StyledHeading>
-        <StyledButton>{buttonText}</StyledButton>
+        <StyledEyebrowText>
+          {eyebrowText ? eyebrowText : 'A code for america brigade'}
+        </StyledEyebrowText>
+        <StyledHeading>
+          {mainHeading ? mainHeading : 'Community service in a digital age'}
+        </StyledHeading>
+        <StyledButton>{buttonText ? buttonText : 'Get Started'}</StyledButton>
       </StyledContentContainer>
       <StyledHeroImageContainer>
-        <StyledHeroImage src={img} alt={imgDescription} />
+        <StyledHeroImage src={img ? img : heroImg} alt={imgDescription} />
       </StyledHeroImageContainer>
     </StyledHeroSection>
   )
