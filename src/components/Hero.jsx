9e717de308900f3b4b6bed0e5917f5187fd2b4c0
Sergen Demir <dmrsrgn@gmail.com>
2022-12-27 11:11:37 -0600

    nest styles and change elements to regular elements from styled components style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,104 +4,126 @@ import heroImg from '../images/skyline.svg'
 
 const StyledHeroSection = styled.section`
   background-color: var(--cfa-hero-bg);
+  display: flex;
+  flex-direction: column;
 
   @media (min-width: 769px) {
-    display: flex;
+    flex-direction: row;
     align-items: center;
     padding-left: 3rem;
     gap: 1.5rem;
   }
-`
 
-const StyledContentContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-  padding: 3rem 1.5rem;
-
-  @media (min-width: 769px) {
-    align-items: flex-start;
-    padding: 4rem 0;
-    gap: 1.5rem;
-    width: 31.188rem;
-    height: 20.625rem;
+  @media (min-width: 1400px) {
+    padding-left: 6rem;
   }
-  @media (min-width: 1200px) {
-    width: 33.75rem;
-  }
-`
 
-const StyledEyebrowText = styled.span`
-  font-style: normal;
-  font-weight: 600;
-  letter-spacing: 0.05em;
-  line-height: 1.313rem;
-  text-align: center;
-  letter-spacing: 0.05em;
-  text-transform: uppercase;
-  height: 1.313rem;
-  color: var(--tints-blizzard-black);
-
-  @media (min-width: 649px) {
-    font-size: 1.125rem;
-  }
-`
-const StyledHeading = styled.h2`
-  text-align: center;
-  font-size: 2rem;
-  font-weight: 700;
-  color: var(--tints-blizzard-black);
-  line-height: 3.438rem;
-  flex: none;
-
-  @media (min-width: 649px) {
-    font-size: 2.625rem;
-  }
-  @media (min-width: 769px) {
-    text-align: left;
-  }
-`
-
-const StyledHeroImageContainer = styled.div`
-  @media (min-width: 769px) {
+  .content-container {
     display: flex;
     flex-direction: column;
-    justify-content: flex-end;
-    align-items: flex-start;
-    padding: 0;
-    gap: 0.625rem;
-    width: 12.375rem;
-    height: 26.313rem;
-    padding-top: 4rem;
+    align-items: center;
+    gap: 1rem;
+    padding: 3rem 1.5rem;
+
+    @media (min-width: 769px) {
+      align-items: flex-start;
+      padding: 4rem 0;
+      gap: 1.5rem;
+      width: 31.188rem;
+      height: 20.625rem;
+    }
+    @media (min-width: 1200px) {
+      width: 33.75rem;
+    }
   }
 
-  @media (min-width: 1200px) {
-    width: 38.25rem;
+  .eyebrow-text {
+    font-style: normal;
+    font-weight: 600;
+    letter-spacing: 0.05em;
+    line-height: 1.313rem;
+    text-align: center;
+    letter-spacing: 0.05em;
+    text-transform: uppercase;
+    height: 1.313rem;
+    color: var(--tints-blizzard-black);
+
+    @media (min-width: 649px) {
+      font-size: 1.125rem;
+    }
+
+    @media (min-width: 769px) {
+      text-align: left;
+    }
   }
-`
 
-const StyledHeroImage = styled.img`
-  @media (min-width: 769px) {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: left;
+  .main-heading {
+    text-align: center;
+    font-size: 2rem;
+    font-weight: 700;
+    color: var(--tints-blizzard-black);
+    line-height: 3.438rem;
+    flex: none;
+
+    @media (min-width: 649px) {
+      font-size: 2.625rem;
+      margin: 0 3rem;
+    }
+    @media (min-width: 769px) {
+      text-align: left;
+      margin: 0;
+    }
+    @media (min-width: 1200px) {
+      padding-right: 1rem;
+    }
   }
-`
 
-const StyledButton = styled.button`
-  padding: 0.5rem 1rem;
-  background-color: var(--white);
-  border: 2px solid #d90000;
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 2.2px;
-  color: var(--tints-blizzard-black);
-  font-weight: 600;
-  font-size: 0.875rem;
+  .hero-image-container {
+    @media (min-width: 769px) {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      align-items: flex-start;
+      padding: 0;
+      gap: 0.625rem;
+      width: 12.375rem;
+      height: 26.313rem;
+      padding-top: 4rem;
+    }
 
-  @media (min-width: 649px) {
-    font-size: 1rem;
+    @media (min-width: 1000px) {
+      width: 35rem;
+    }
+
+    @media (min-width: 1200px) {
+      width: 38.25rem;
+      flex-direction: row;
+      justify-content: center;
+    }
+  }
+
+  .hero-image {
+    @media (min-width: 769px) {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: left;
+    }
+  }
+
+  .button {
+    padding: 0.5rem 1rem;
+    background-color: var(--white);
+    border: 2px solid #d90000;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 2.2px;
+    color: var(--tints-blizzard-black);
+    font-weight: 600;
+    font-size: 0.875rem;
+
+    @media (min-width: 649px) {
+      font-size: 1rem;
+    }
   }
 `
 
@@ -114,18 +136,24 @@ const Hero = ({
 }) => {
   return (
     <StyledHeroSection>
-      <StyledContentContainer>
-        <StyledEyebrowText>
+      <div className="content-container">
+        <span className="eyebrow-text">
           {eyebrowText ? eyebrowText : 'A code for america brigade'}
-        </StyledEyebrowText>
-        <StyledHeading>
+        </span>
+        <h2 className="main-heading">
           {mainHeading ? mainHeading : 'Community service in a digital age'}
-        </StyledHeading>
-        <StyledButton>{buttonText ? buttonText : 'Get Started'}</StyledButton>
-      </StyledContentContainer>
-      <StyledHeroImageContainer>
-        <StyledHeroImage src={img ? img : heroImg} alt={imgDescription} />
-      </StyledHeroImageContainer>
+        </h2>
+        <button className="button">
+          {buttonText ? buttonText : 'Get Started'}
+        </button>
+      </div>
+      <div className="hero-image-container">
+        <img
+          src={img ? img : heroImg}
+          alt={imgDescription}
+          className="hero-image"
+        />
+      </div>
     </StyledHeroSection>
   )
 }
