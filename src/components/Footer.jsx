0a2fedc71d8b3fd8d62671fefb57c2aecf4eda3b
Sergen Demir <dmrsrgn@gmail.com>
2023-01-05 10:09:37 -0600

    style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,69 @@ import styled from 'styled-components'
 import logo from '../images/cfc_logo_short.svg'
 
 const StyledFooter = styled.footer`
-  width: 14rem;
+  width: 100%;
+  padding: 1rem 0;
+  display: block;
+
+  .footer-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 3rem;
+    gap: 1rem;
+
+    @media (min-width: 768px) {
+      flex-direction: row;
+      gap: 0;
+    }
+
+    @media (min-width: 1200px) {
+      padding: 0 6rem;
+    }
+  }
+
+  .footer-logo {
+    width: 4.75rem;
+    height: auto;
+  }
+
+  .footer-right {
+    text-align: center;
+
+    @media (min-width: 768px) {
+      text-align: right;
+      max-width: 75%;
+    }
+
+    a {
+      color: var(--cfa-blue-dark);
+      text-decoration: none;
+    }
+  }
 `
 const Footer = () => {
   return (
     <StyledFooter>
-      <img src={logo} alt="Code for Chicago Logo Icon" />
-      <p>
-        <a href="https://codeforamerica.org/" target="_blank" rel="noreferrer">
-          Code for America Labs, Inc.
-        </a>{' '}
-        is a non-partisan, non-political 501(c)(3) charitable organization
-      </p>
+      <div className="footer-content">
+        <img
+          src={logo}
+          alt="Code for Chicago Logo Icon"
+          className="footer-logo"
+        />
+        <div className="footer-right">
+          <p>
+            <a
+              href="https://codeforamerica.org/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Code for America Labs, Inc.
+            </a>{' '}
+            is a non-partisan, non-political 501(c)(3) charitable organization
+          </p>
+        </div>
+      </div>
     </StyledFooter>
   )
 }
