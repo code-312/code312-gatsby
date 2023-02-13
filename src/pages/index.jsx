1428e93537797c2619db132065d81a15ed736b4c
Doug Leinen <doug@geletkaplus.com>
2023-02-12 19:15:27 -0600

    content section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import Card from '../components/Card'
-import Hero from '../components/Hero'
 import Layout from '../components/Layout'
+import Hero from '../components/Hero'
+import ContentSection from '../components/ContentSection'
+import Organizations from '../images/Organizations.png'
 
 const IndexPage = () => {
   return (
     <Layout>
-      <h2>homepage</h2>
       <Hero />
-      <Card />
+      <ContentSection
+        background={'var(--blizzard-blue)'}
+        eyebrowText={'Organizations'}
+        mainHeading={
+          'Are you a Chicagoland nonprofit that needs some tech help?'
+        }
+        content={
+          "Have an idea how to make your community better? You've come to the right place. Either contact us or come to one of our meetups to get the conversation started."
+        }
+        linkText={'Learn More'}
+        linkUrl={'/'}
+        imgUrl={Organizations}
+        imageDescription={'Organization receiving help from Code for Chicago'}
+      />
     </Layout>
   )
 }
