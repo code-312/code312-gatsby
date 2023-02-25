d95b71872ea1d51bd128f8ae55c1017d8de3e008
Doug Leinen <doug@geletkaplus.com>
2023-02-25 12:13:00 -0600

    set flex, center horizontally

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,11 +4,16 @@ import styled from 'styled-components'
 const StyledHeading = styled.div`
   color: var(--tints-blizzard-black);
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin: 0 auto;
   padding: 4rem 1.5rem 3rem;
   max-width: 35.1875rem;
 
   .eyebrow-1 {
     text-transform: uppercase;
+    padding-bottom: 0.5rem;
   }
 `
 
