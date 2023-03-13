ba349a30f17f59831dc01122ca1cf58497bd5bbf
Jon Scarpelli <scarpelli.jon@gmail.com>
2023-03-13 11:59:08 -0500

    added iconTiles

--- src/pages/join.jsx
+++ src/pages/join.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Card from '../components/Card'
-import CardBlock from '../components/CardBlock'
+import IconTile from '../components/IconTile'
 import Hero from '../components/Hero'
 import Heading from '../components/Heading'
 import Layout from '../components/Layout'
+import ContentSection from '../components/ContentSection'
+import codeOfConduct from '../images/code-of-conduct.svg'
+import TestImage from '../images/TestImage.png'
 
 const JoinPage = () => {
   return (
@@ -19,12 +22,59 @@ const JoinPage = () => {
         description="this content describes this section"
         headingLevel="3"
       />
-            <CardBlock>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-      </CardBlock>
+      <section className="icon-container">
+        <IconTile
+          icon={codeOfConduct}
+          title={'Code of Conduct'}
+          description={'We ensure we keep teamwork safe and fun.'}
+        />
+        <IconTile
+          icon={codeOfConduct}
+          title={'Ask Questions'}
+          description={'Don’t be afraid to ask someone questions!'}
+        />
+        <IconTile
+          icon={codeOfConduct}
+          title={'Availability'}
+          description={
+            'We know life happens so let your team know if you have capacity'
+          }
+        />
+        <IconTile
+          icon={codeOfConduct}
+          title={'Hand-Off Tasks'}
+          description={
+            'Ensure your work continues and transfer your tasks to another!'
+          }
+        />
+      </section>
+      <ContentSection
+        eyebrowText={'Eyebrow Text Goes Here'}
+        mainHeading={'Main Heading Goes Here'}
+        content={'Content paragraph goes here. Lorem Ipsum.'}
+        linkUrl={'/components'}
+        linkText={'To Components Page'}
+        imgUrl={TestImage}
+        imageDescription={'image for testing components'}
+      />
+      <ContentSection
+        eyebrowText={'Eyebrow Text Goes Here'}
+        mainHeading={'Main Heading Goes Here'}
+        content={'Content paragraph goes here. Lorem Ipsum.'}
+        linkUrl={'/components'}
+        linkText={'To Components Page'}
+        imgUrl={TestImage}
+        imageDescription={'image for testing components'}
+      />
+      <ContentSection
+        eyebrowText={'Eyebrow Text Goes Here'}
+        mainHeading={'Main Heading Goes Here'}
+        content={'Content paragraph goes here. Lorem Ipsum.'}
+        linkUrl={'/components'}
+        linkText={'To Components Page'}
+        imgUrl={TestImage}
+        imageDescription={'image for testing components'}
+      />
     </Layout>
   )
 }
