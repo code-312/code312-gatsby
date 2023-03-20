6a1c5181c2f4e7d0a2780304ca6dd5f53981c9b5
Edgar Cuarezma <ecuarezma@gmail.com>
2023-03-19 20:19:47 -0500

    refactor code

--- src/components/CardBlock.jsx
+++ src/components/CardBlock.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledCardContainer = styled.section`
+  const StyledCardContainer = styled.section`
   display: flex;
   flex-wrap: wrap;
   justify-content:center;
@@ -15,21 +15,24 @@ const StyledCardContainer = styled.section`
       flex: 0 1 45%;
     }
     @media (min-width: 769px) {
-      flex: 0 1 ${props =>  props.cardQty > 3 ? '30%' : '45%' };
+      flex: 0 1 ${({cardQty}) => cardQty > 3 ? '30%' : '45%'};
       &:last-child{
         justify-self:center;
       }
     }
     @media (min-width: 1200px) {
-      flex: 0 0 ${props =>  props.cardQty > 3 ? '20%' : '30%'};
+      flex: 0 0 ${({cardQty}) => cardQty > 3 ? '20%' : '30%'};
     }
   }
 `
 
 const CardBlock = ({ children }) => {
   
+
   return (
-    <StyledCardContainer cardQty={children.length} />
+    <StyledCardContainer cardQty={children.length}>
+        {children}
+    </StyledCardContainer>
   )
 }
 
