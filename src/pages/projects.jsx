8d99383d2e0d6fc5dc652889f152566cd15b203f
Edgar Cuarezma <ecuarezma@gmail.com>
2023-04-19 20:43:47 -0500

    lay out components with corresponding text fields and styling

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,18 +1,24 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/Layout"
-import Card from "../components/Card"
-import CardBlock from "../components/CardBlock"
-import Heading from "../components/Heading"
-import Hero from "../components/Hero"
+import Layout from '../components/Layout'
+import Card from '../components/Card'
+import CardBlock from '../components/CardBlock'
+import Heading from '../components/Heading'
+import Hero from '../components/Hero'
 
+import styled from 'styled-components'
+
+const StyledSection = styled.div`
+  background-color: var(--light-grey);
+  padding-bottom: 4rem;
+`
 
 const Projects = () => {
   return (
     <Layout>
       <Hero
-        eyebrowText={'Eyebrow Text Goes Here'}
-        mainHeading={'Main Heading Goes Here'}
+        eyebrowText={'Active Projects'}
+        mainHeading={'Explore active projects to get involved'}
       />
       <Heading
         browText="NEEDS VOLUNTEERS"
@@ -26,18 +32,21 @@ const Projects = () => {
         <Card></Card>
         <Card></Card>
       </CardBlock>
-      <Heading
-        browText="TEAMS AT CAPACITY"
-        headingText="Not Recruiting"
-        description="These projects are active, but don’t have room for additional volunteers."
-        headingLevel="3"
-      />
-      <CardBlock>
-        <Card></Card>
-      </CardBlock>
+      <StyledSection>
+        <Heading
+          browText="TEAMS AT CAPACITY"
+          headingText="Not Recruiting"
+          description="These projects are active, but don’t have room for additional volunteers."
+          headingLevel="3"
+        />
+        <CardBlock>
+          <Card></Card>
+        </CardBlock>
+      </StyledSection>
+
       <Hero />
     </Layout>
   )
 }
 
-export default Projects;
+export default Projects
