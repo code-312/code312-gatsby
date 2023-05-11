a6fa4a175ee0c84aafeed8cd7d5f92941ce5020d
Dior.Shelton <dior.shelton@gmail.com>
2023-05-10 20:13:18 -0400

    Add Quote and FAQ component to About page along with styling for Quote.jsx

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,13 +1,54 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Quote = ({ quote, author, source }) => (
-  <blockquote>
-    <p className="quote">{quote}</p>
-    <cite className="heading-3">
-      {author}, <cite></cite>
-      {source}
-    </cite>
-  </blockquote>
-)
+const StyledQuote = styled.section`
+  background: ${(props) => props.background || 'var(--light-grey)'};
 
+  .quote-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 4rem 3rem;
+    gap: 1.5rem;
+    width: 75rem;
+    height: 22.75rem;
+  }
+  .blockquote {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0rem;
+    gap: 1.5rem;
+    width: 33.69rem;
+    height: 14.75rem; 
+    line-height: 2.63rem;
+    font-size: 1.75rem;
+    font-weight: 300;
+    font-style: normal;
+  }
+  .heading-3 {
+    flex: none;
+    align-self: stretch;
+    letter-spacing: 0.05em;
+    text-transform: uppercase;
+    font-weight: 600;
+  }
+`
+
+const Quote = ({ quote, author, source, background }) => {
+  return (
+    <StyledQuote background={background}>
+      <div className='quote-wrapper'>
+        <blockquote className='blockquote'>
+          <p className="quote">{quote}</p>
+          <cite className="heading-3">
+            {author},
+            {source}
+          </cite>
+        </blockquote>
+      </div>
+    </StyledQuote>
+  )
+}
 export default Quote
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import FAQSection from '../components/FAQSection'
 import Quote from '../components/Quote'
-
+import Button from '../components/Button'
 const AboutPage = () => {
   return (
     <Layout>
@@ -26,12 +26,47 @@ const AboutPage = () => {
         </p>
       </FAQSection>
       <Quote
+        background={'var(--light-grey)'}
         quote={
-        '"Those of us who work in civic tech want public digital goods to be as good as the ones made by commercial entities like Apple or Google—and we want public digital infrastructure to be as good, too."'
+          '"Those of us who work in civic tech want public digital goods to be as good as the ones made by commercial entities like Apple or Google—and we want public digital infrastructure to be as good, too."'
         }
         author={'Cyd Harrell'}
-        source={'A Civic Technologist’s Practice Guide'}
+        source={' A Civic Technologist’s Practice Guide'}
       />
+      <FAQSection
+        background={'var(--white)'}
+        eyebrowText={'WHAT IS CODE FOR CHICAGO?'}
+        mainHeading={
+          'Code for Chicago operates like a pro-bono digital consultancy'
+        }
+      >
+        <div className="eyebrow-header-group">
+          <h3 className="h3-eyebrow">Operating Shoulder-to-Shoulder</h3>
+          <p className="text-content">
+            When we start a new project we typically partner up with a local
+            organization. Since we aren't our users, this ensures we are
+            effectively addressing the needs of the community and not operating
+            from our own perspective as practitioners.
+          </p>
+          <h3 className="h3-eyebrow">Establishing Our Process</h3>
+          <p className="text-content">
+            We begin by identifying the needs of the organization, drafting a
+            problem statement, and collaborating throughout the project to
+            ensure needs are met.
+          </p>
+          <h3 className="h3-eyebrow">Volunteer Work</h3>
+          <p className="text-content">
+            Our process requires an army of volunteers of various skill sets who
+            are all on a mission to improve technology for the people.
+            Volunteers typically join to try out civic tech, network with
+            like-minded people, and learn new skills.
+          </p>
+          <div className="button-group">
+           <Button text={'Volunteer With Us'}></Button>
+           <Button text={'Partner with Us'}></Button>
+          </div>
+        </div>
+      </FAQSection>
     </Layout>
   )
 }
