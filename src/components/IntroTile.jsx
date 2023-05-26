cc75bc3e1602bcd8e3ef1154e12da4d05d2b5374
Dior.Shelton <dior.shelton@gmail.com>
2023-05-26 15:55:02 -0400

    Add jsx for IntroTile and images

---
+++ src/components/IntroTile.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledIntroTile = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0rem 1rem;
+  width: 16.0625rem;
+  height: 12.4375rem;
+  background: var(--white);
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+  border-radius: 0.5rem;
+
+  .details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0rem;
+    gap: 0.5rem;
+  }
+`
+
+const IntroTile = ({imageSrc, description, memberName, positionTitle}) => {
+  return (
+    <StyledIntroTile>
+      <div className="card">
+        <img className="cardImage"
+          src={imageSrc} alt={description} />
+        <div className="details">
+          <p className="name">{memberName}</p>
+          <p className="title">{positionTitle}</p>
+        </div>
+      </div>
+    </StyledIntroTile>
+  )
+}
+export default IntroTile
--- src/pages/components.jsx
+++ src/pages/components.jsx
@@ -17,7 +17,9 @@ import Heading from '../components/Heading'
 import LongtextInput from '../components/LongTextInput'
 import Checklist from '../components/Checklist'
 import Quote from '../components/Quote'
-
+import IntroTile from '../components/IntroTile'
+import TilePhoto from "../../images/uploads/allie_serd.png"
+// /Users/dior/cfc/code-for-chicago-gatsby/images/uploads/allie_serd.png
 const Components = () => {
   return (
     <Layout>
@@ -80,6 +82,8 @@ const Components = () => {
         title={'Title'}
         description={'Description goes Here'}
       />
+      <IntroTile memberName={"Allie Serd"}
+        imageSrc={TilePhoto} positionTitle={"Volunteer Lead"}></IntroTile>
     </Layout>
   )
 }
