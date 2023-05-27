87f0289b61372966dbb3d7b0ff81993876f10e0a
Dior.Shelton <dior.shelton@gmail.com>
2023-05-27 18:21:47 -0400

    Add CSS for for image and font-weight

--- src/components/IntroTile.jsx
+++ src/components/IntroTile.jsx
@@ -12,13 +12,21 @@ const StyledIntroTile = styled.div`
   background: var(--white);
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
   border-radius: 0.5rem;
-
+  .cardImage {
+    height: 6rem;
+    width: 6rem;
+    left: 4.0312rem;
+    top: 1rem;
+  }
   .details {
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 0rem;
     gap: 0.5rem;
+  }
+  .name {
+    font-weight: 700;
   }
 `
 
@@ -30,7 +38,7 @@ const IntroTile = ({imageSrc, description, memberName, positionTitle}) => {
           src={imageSrc} alt={description} />
         <div className="details">
           <p className="name">{memberName}</p>
-          <p className="title">{positionTitle}</p>
+          <p className="title">{positionTitle}</p> 
         </div>
       </div>
     </StyledIntroTile>
