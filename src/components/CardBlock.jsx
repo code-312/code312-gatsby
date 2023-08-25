e9234fdfc45dc872b51838e5a703e80fd919b249
Edgar Cuarezma <ecuarezma@gmail.com>
2023-08-25 17:13:57 -0500

    update styling on card block

--- src/components/CardBlock.jsx
+++ src/components/CardBlock.jsx
@@ -5,33 +5,16 @@ const StyledCardContainer = styled.section`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  max-width: 64rem;
+  margin: 0 auto;
   padding-bottom: 4rem;
-  article {
-    flex: 1 1 100%;
-    @media (min-width: 320px) {
-      flex: 0 1 45%;
-    }
-    @media (min-width: 650px) {
-      flex: 0 1 45%;
-    }
-    @media (min-width: 769px) {
-      flex: 0 1 ${({ cardQty }) => (cardQty > 3 ? '30%' : '45%')};
-      &:last-child {
-        justify-self: center;
-      }
-    }
-    @media (min-width: 1200px) {
-      flex: 0 0 ${({ cardQty }) => (cardQty > 3 ? '20%' : '30%')};
-    }
+  .card-item {
+    display: flex;
   }
 `
 
-const CardBlock = ({ children }) => {
-  return (
-    <StyledCardContainer cardQty={children.length}>
-      {children}
-    </StyledCardContainer>
-  )
-}
+const CardBlock = ({ children }) => (
+  <StyledCardContainer>{children}</StyledCardContainer>
+)
 
 export default CardBlock
