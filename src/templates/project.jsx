801c61d6c2d441a9ac62b71b1fce1ebc82622f6c
Doug Leinen <doug@geletkaplus.com>
2023-08-28 19:27:47 -0500

    start project detail page

--- src/templates/project.jsx
+++ src/templates/project.jsx
@@ -1,14 +1,49 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import styled from 'styled-components'
+
+const StyledProject = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+`
+
+const StyledLeftColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  max-width: 22rem;
+`
+
+const StyledRightColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  max-width: 39.5rem;
+`
 
 export default function Project({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
-      <div>
-        <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <div className="width-wrapper">
+        <StyledProject>
+          <StyledLeftColumn>
+            <GatsbyImage
+              image={post.frontmatter.thumbnail.childImageSharp.gatsbyImageData}
+              alt={post.frontmatter.imgtext}
+            />
+          </StyledLeftColumn>
+          <StyledRightColumn>
+            <h1>{post.frontmatter.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          </StyledRightColumn>
+        </StyledProject>
       </div>
     </Layout>
   )
