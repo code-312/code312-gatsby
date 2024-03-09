1e386400c339a0f0025de1878faf018fde097c88
Doug Leinen <doug@geletkaplus.com>
2024-03-09 08:23:03 -0600

    icons and cards in place

--- src/pages/join.jsx
+++ src/pages/join.jsx
@@ -5,16 +5,24 @@ import Hero from '../components/Hero'
 import Heading from '../components/Heading'
 import Layout from '../components/Layout'
 import AlternatingCard from '../components/AlternatingCard'
-import code from '../images/join-us/code.svg'
-import question from '../images/join-us/question-mark.svg'
-import available from '../images/join-us/available.svg'
-import active from '../images/join-us/active.svg'
+
+import active from '../images/icons/active.svg'
+import calendardays from '../images/icons/calendardays.svg'
+import code from '../images/icons/code.svg'
+import codeconduct2 from '../images/icons/codeconduct2.svg'
+import datascientists from '../images/icons/datascientists.svg'
+import designers from '../images/icons/designers.svg'
+import ghost from '../images/icons/ghost.svg'
+import handOff from '../images/icons/hand-off.svg'
+import lightbulb from '../images/icons/lightbulb.svg'
+import ops from '../images/icons/ops.svg'
+import researchers from '../images/icons/researchers.svg'
 import slack from '../images/slack.svg'
-import codeconduct2 from '../images/join-us/codeconduct2.svg'
-import handOff from '../images/join-us/hand-off.svg'
+
 import zoom from '../images/join-us/zoom.png'
 import meet from '../images/join-us/meet.jpeg'
 import sloth from '../images/join-us/sloth.jpeg'
+
 import styled from 'styled-components'
 
 const HeadingWrapper = styled.section`
@@ -23,27 +31,24 @@ const HeadingWrapper = styled.section`
 const JoinPage = () => {
   const icons = [
     {
-      img: code,
+      img: calendardays,
       title: 'Attend Meetings',
       description: 'All project teams have a regular meeting schedule.',
     },
     {
-      img: question,
+      img: ghost,
       title: "Don't ghost us",
       description: 'We know life happens so keep your team updated.',
-      link: 'View Projects',
     },
     {
-      img: available,
+      img: lightbulb,
       title: 'Take initiative',
       description: 'We work best when we fill in the gaps in the workflow.',
-      link: 'View Conduct',
     },
     {
       img: handOff,
       title: 'Hand-Off Tasks',
       description: 'When you can’t participate anymore then let us know!',
-      link: 'Join Slack',
     },
   ]
 
@@ -54,7 +59,7 @@ const JoinPage = () => {
       description:
         'Check out the projects that are actively recruiting volunteers.',
       btnText: 'View Projects',
-      linkUrl: '/',
+      linkUrl: '/projects',
     },
     {
       img: codeconduct2,
@@ -69,46 +74,47 @@ const JoinPage = () => {
       description:
         'Check out our Slack where all of our collaboration happens.',
       btnText: 'Join Slack',
-      linkUrl: '/',
+      linkUrl:
+        'https://join.slack.com/t/code312/shared_invite/zt-2acnn36rl-kNFFP9E_NqxWORqHfBIO2w',
     },
   ]
 
   const icons3 = [
     {
-      img: active,
+      img: datascientists,
       title: 'Data Analysts',
       description:
         'Use data tools (e.g. SQL) to gather, clean, and interpret data.',
     },
     {
-      img: codeconduct2,
+      img: code,
       title: 'Developers',
       description:
         'From React to PostgreSQL, help us on the front and back-end.',
     },
     {
-      img: slack,
+      img: designers,
       title: 'Designers',
       description:
         'We facilitate the entire design-thinking process from UX to UI.',
     },
     {
-      img: slack,
+      img: researchers,
       title: 'Researchers',
       description: 'Talk to actual users to help us validate our solutions.',
     },
     {
-      img: slack,
+      img: ops,
       title: 'Operations',
       description: 'We need people behind the scenes to keep us organized.',
     },
     {
-      img: slack,
+      img: handOff,
       title: 'Project Managers',
       description: 'Talk to our partners and manage the project teams.',
     },
     {
-      img: slack,
+      img: lightbulb,
       title: 'Product Managers',
       description: 'Identify which features to implement and plan work.',
     },
@@ -159,11 +165,13 @@ const JoinPage = () => {
         title="Projects vary and timelines change"
         description="We operate on our volunteer’s capacity so our pace is slow. Depending on scope and volunteer contribution, we’ve completed projects anywhere between 3 months and 1 year."
       />
-      <Heading
-        browText="How can you contribute?"
-        headingText="Our teams are cross-functional and operate in different industries"
-        description="We use design thinking to resolve technical challenges."
-      />
+      <HeadingWrapper>
+        <Heading
+          browText="How can you contribute?"
+          headingText="Our teams are cross-functional and operate in different industries"
+          description="We use design thinking to resolve technical challenges."
+        />
+      </HeadingWrapper>
       <IconGrid icons={icons3} />
       <HeadingWrapper>
         <Heading
@@ -174,9 +182,9 @@ const JoinPage = () => {
       </HeadingWrapper>
       <IconGrid icons={icons2} />
       <Hero
-        mainHeading="Have you completed the pre-onboarding checklist?"
-        buttonText="Start Onboarding"
-        link="/"
+        mainHeading="Step 2"
+        buttonText="Complete our onboarding form"
+        link="https://docs.google.com/presentation/d/1NrxKFWd3hhG5gn1Uyvaz-nM34kREHzquW_p4TwzIfCk/edit?usp=sharing"
         arrow
       />
     </Layout>
